d1f40fd79ec1c2c716cc2bb601fc6683aa43f883
Samuel Pitoňák <sampittko@gmail.com>
2023-01-19 10:22:51 +0100

    chore: updates title and hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,16 +9,23 @@ import blurCyanImage from '@/images/blur-cyan.png'
 import blurIndigoImage from '@/images/blur-indigo.png'
 
 const codeLanguage = 'javascript'
-const code = `export default {
-  strategy: 'predictive',
-  engine: {
-    cpus: 12,
-    backups: ['./storage/cache.wtf'],
-  },
-}`
+const code = `const LowCarbonComponent =
+  React.lazy(() => import('./LowCarbonComponent'))
+
+const ModerateCarbonComponent =
+  React.lazy(() => import('./ModerateCarbonComponent'))
+
+const HighCarbonComponent =
+  React.lazy(() => import('./HighCarbonComponent'))
+
+export default withSui([
+  LowCarbonComponent,
+  ModerateCarbonComponent,
+  HighCarbonComponent
+])`
 
 const tabs = [
-  { name: 'cache-advance.config.js', isActive: true },
+  { name: 'CarbonComponent.jsx', isActive: true },
   { name: 'package.json', isActive: false },
 ]
 
@@ -48,16 +55,15 @@ export function Hero() {
               priority
             />
             <div className="relative">
-              <p className="inline bg-gradient-to-r from-indigo-200 via-sky-400 to-indigo-200 bg-clip-text font-display text-5xl tracking-tight text-transparent">
-                Never miss the cache again.
+              <p className="inline bg-gradient-to-r from-lime-200 via-green-400 to-lime-200 bg-clip-text font-display text-5xl tracking-tight text-transparent">
+                UIs redefined for the better.
               </p>
               <p className="mt-3 text-2xl tracking-tight text-slate-400">
-                Cache every single thing your app could ever do ahead of time,
-                so your code never even has to run at all.
+                Principally different approach to building apps where the planet benefits the most and users remain in control.
               </p>
               <div className="mt-8 flex gap-4 md:justify-center lg:justify-start">
-                <Button href="/">Get started</Button>
-                <Button href="/" variant="secondary">
+                <Button href="/#getting-started">Get started</Button>
+                <Button href="https://github.com/sustainableui/sui-headless-react" variant="secondary">
                   View on GitHub
                 </Button>
               </div>
@@ -86,10 +92,10 @@ export function Hero() {
                 unoptimized
                 priority
               />
-              <div className="absolute inset-0 rounded-2xl bg-gradient-to-tr from-sky-300 via-sky-300/70 to-blue-300 opacity-10 blur-lg" />
-              <div className="absolute inset-0 rounded-2xl bg-gradient-to-tr from-sky-300 via-sky-300/70 to-blue-300 opacity-10" />
+              <div className="absolute inset-0 rounded-2xl bg-gradient-to-tr from-green-300 via-green-300/70 to-blue-300 opacity-10 blur-lg" />
+              <div className="absolute inset-0 rounded-2xl bg-gradient-to-tr from-green-300 via-green-300/70 to-blue-300 opacity-10" />
               <div className="relative rounded-2xl bg-[#0A101F]/80 ring-1 ring-white/10 backdrop-blur">
-                <div className="absolute -top-px left-20 right-11 h-px bg-gradient-to-r from-sky-300/0 via-sky-300/70 to-sky-300/0" />
+                <div className="absolute -top-px left-20 right-11 h-px bg-gradient-to-r from-green-300/0 via-green-300/70 to-green-300/0" />
                 <div className="absolute -bottom-px left-11 right-20 h-px bg-gradient-to-r from-blue-400/0 via-blue-400 to-blue-400/0" />
                 <div className="pl-4 pt-4">
                   <TrafficLightsIcon className="h-2.5 w-auto stroke-slate-500/30" />
@@ -100,7 +106,7 @@ export function Hero() {
                         className={clsx(
                           'flex h-6 rounded-full',
                           tab.isActive
-                            ? 'bg-gradient-to-r from-sky-400/30 via-sky-400 to-sky-400/30 p-px font-medium text-sky-300'
+                            ? 'bg-gradient-to-r from-green-400/30 via-green-400 to-green-400/30 p-px font-medium text-green-300'
                             : 'text-slate-500'
                         )}
                       >
--- src/pages/index.md
+++ src/pages/index.md
@@ -1,6 +1,6 @@
 ---
 title: Getting started
-pageTitle: CacheAdvance - Never miss the cache again.
+pageTitle: Sustainable UI - Build UIs with sustainability at core.
 description: Cache every single thing your app could ever do ahead of time, so your code never even has to run at all.
 ---
 
