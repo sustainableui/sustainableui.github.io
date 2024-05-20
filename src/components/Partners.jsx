e4cc4c85f9c1b2ee44a4e48e412ad1a33669f893
Samuel Pitonak <sampittko@gmail.com>
2024-05-20 09:49:59 +0200

    chore: adds edi logo

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,10 +1,14 @@
-import AdoraFoundation from "@/components/logos/AdoraFoundation";
+import AdoraFoundation from '@/components/logos/AdoraFoundation'
+import Edi from '@/components/logos/edi'
 
 export default function Partners() {
-    return (
-        <>
-            <h2 className="text-center">Our partners</h2>
-            <AdoraFoundation />
-        </>
-    )
+  return (
+    <>
+      <h2 className="text-center">Our partners</h2>
+      <div className="not-prose mt-10 flex flex-row items-center justify-center gap-10">
+        <Edi />
+        <AdoraFoundation />
+      </div>
+    </>
+  )
 }
